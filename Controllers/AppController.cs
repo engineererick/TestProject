d343f9d06d94cd8b846b43fcca554745350f45be
engineererick <engineererick@hotmail.com>
2021-09-03 15:08:55 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/AppController.cs
@@ -0,0 +1,99 @@
+﻿using Intuit.Ipp.OAuth2PlatformClient;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Net;
+using Intuit.Ipp.Core;
+using Intuit.Ipp.Data;
+using Intuit.Ipp.QueryFilter;
+using Intuit.Ipp.Security;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MvcCodeFlowClientManual.Controllers
+{
+    public class AppController : Controller
+    {
+        /*
+            Initialization of auth2 object and strings.
+            Static because it will never change in the current Session
+         */
+        public static string clientid = ConfigurationManager.AppSettings["clientid"];
+        public static string clientsecret = ConfigurationManager.AppSettings["clientsecret"];
+        public static string redirectUrl = ConfigurationManager.AppSettings["redirectUrl"];
+        public static string environment = ConfigurationManager.AppSettings["appEnvironment"];
+
+        public static OAuth2Client auth2Client = new OAuth2Client(clientid, clientsecret, redirectUrl, environment);
+
+        
+        public ActionResult Index()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            Session.Clear();
+            Session.Abandon();
+            Request.GetOwinContext().Authentication.SignOut("Cookies");
+            return View();
+        }
+
+        /*
+            An authorization mode is needed to get the connection to Quickbooks account
+         */
+        public ActionResult InitiateAuth()
+        {
+            List<OidcScopes> scopes = new List<OidcScopes>();
+            scopes.Add(OidcScopes.Accounting);
+            var auth = auth2Client.GetAuthorizationURL(scopes);
+            return Redirect(auth);
+        }
+
+        /*
+            The apps can connect to Quickbooks API once the token is received.
+         */
+        public ActionResult ApiCallService()
+        {
+            if (Session["realmId"] != null)
+            {
+                string realmId = Session["realmId"].ToString();
+                try
+                {
+                    /*
+                        A ServiceContext is created to request the API, also an access token and realmId are created
+                     */
+                    var principal = User as ClaimsPrincipal;
+                    OAuth2RequestValidator oauthValidator = new OAuth2RequestValidator(principal.FindFirst("access_token").Value);
+
+                    ServiceContext serviceContext = new ServiceContext(realmId, IntuitServicesType.QBO, oauthValidator);
+                    serviceContext.IppConfiguration.MinorVersion.Qbo = "23";
+
+                    //According to the requested, the Customer entitie is called from the API and receive all the customers
+                    //into a List of customers
+                    QueryService<Customer> querySvc = new QueryService<Customer>(serviceContext);
+                    List<Customer> companyInfo = querySvc.ExecuteIdsQuery("SELECT * FROM Customer").ToList();
+
+                    return View("ApiCallService", companyInfo);
+                }
+                catch (Exception ex)
+                {
+                    return View("ApiCallService", null);
+                }
+            }
+            else
+                return View("ApiCallService", null);
+        }
+
+        public ActionResult Error()
+        {
+            return View("Error");
+        }
+
+        
+        public ActionResult Tokens()
+        {
+            return View("Tokens");
+        }
+    }
+}
---
+++ Controllers/CallbackController.cs
@@ -0,0 +1,62 @@
+﻿using Intuit.Ipp.OAuth2PlatformClient;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCodeFlowClientManual.Controllers
+{
+    public class CallbackController : Controller
+    {
+        /*
+            Request the auth code(code) and Quickbooks account ID (realmId) and await
+            the token to authorize the callback
+         */
+        public async Task<ActionResult> Index()
+        {
+            var state = Request.QueryString["state"];
+            if (state.Equals(AppController.auth2Client.CSRFToken, StringComparison.Ordinal))
+                ViewBag.State = state + " (valid)";
+            else ViewBag.State = state + " (invalid)";
+
+            string code = Request.QueryString["code"] ?? "none";
+            string realmId = Request.QueryString["realmId"] ?? "none";
+            await GetAuthTokensAsync(code, realmId);
+
+            ViewBag.Error = Request.QueryString["error"] ?? "none";
+
+            return RedirectToAction("Tokens", "App");
+        }
+
+        /*
+            Stored the realmId and code in Claims to be used in the current session.
+            Also refresh tokens time to avoid the end of the session.
+         */
+        private async Task GetAuthTokensAsync(string code, string realmId)
+        {
+            if (realmId != null) Session["realmId"] = realmId;
+
+            Request.GetOwinContext().Authentication.SignOut("TempState");
+            var tokenResponse = await AppController.auth2Client.GetBearerTokenAsync(code);
+
+            var claims = new List<Claim>();
+
+            if (Session["realmId"] != null) claims.Add(new Claim("realmId", Session["realmId"].ToString()));
+            
+            if (!string.IsNullOrWhiteSpace(tokenResponse.AccessToken))
+                claims.Add(new Claim("access_token", tokenResponse.AccessToken));
+                claims.Add(new Claim("access_token_expires_at", (DateTime.Now.AddSeconds(tokenResponse.AccessTokenExpiresIn)).ToString()));
+            
+
+            if (!string.IsNullOrWhiteSpace(tokenResponse.RefreshToken))
+                claims.Add(new Claim("refresh_token", tokenResponse.RefreshToken));
+                claims.Add(new Claim("refresh_token_expires_at", (DateTime.Now.AddSeconds(tokenResponse.RefreshTokenExpiresIn)).ToString()));
+            
+
+            var id = new ClaimsIdentity(claims, "Cookies");
+            Request.GetOwinContext().Authentication.SignIn(id);
+        }
+    }
+}
